eeca5712471a5739f9c9fee8f6df6f7c35bab50e
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-15 10:45:01 +0700

    pipeline/data_preproses(handling outliers): handling outliers untuk df_train dan df_test
    # Description
    handling outliers untuk dataframe df_train dan df_test, ini dipisah dikarenakan pencegahan data leaked
    
    #  Changes :
    
    1. pipeline untuk handling outliers sudah dilakukan untuk dua data set terpisah yaitu df_train dan df_test.
    
    #  Results / Notes :
    
    1. dikarenakan ini output nya ada dua jenis dataframe, mungkin akan sedikit lama untuk preproses handling outleiers nya.

--- src/bank_transaction_user_classification/pipelines/data_preproses/nodes.py
+++ src/bank_transaction_user_classification/pipelines/data_preproses/nodes.py
@@ -2,3 +2,47 @@
 This is a boilerplate pipeline 'data_preproses'
 generated using Kedro 1.0.0
 """
+
+import pandas as pd
+import numpy as np
+import typing as tp
+
+
+def handle_outliers_iqr(df, columns, k=1.5, method="remove"):
+    df_copy = df.copy()
+    mask = pd.Series(False, index=df.index)  # semua False dulu
+
+    for col in columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower = Q1 - k * IQR
+        upper = Q3 + k * IQR
+
+        if method == "capping":
+            # Ganti nilai outlier dengan batas bawah/atas
+            df_copy[col] = np.where(
+                df[col] < lower, lower, np.where(df[col] > upper, upper, df[col])
+            )
+
+        else:
+            # Tandai outlier untuk detect/remove
+            mask |= (df[col] < lower) | (df[col] > upper)
+
+    if method == "detect":
+        return df[mask]  # hanya outliers
+    elif method == "remove":
+        return df[~mask]  # data tanpa outliers
+    elif method == "capping":
+        return df_copy  # data dengan capping
+    else:
+        raise ValueError("method harus salah satu dari: 'detect', 'remove', 'capping'")
+
+
+def handling_outliers(df: pd.DataFrame, params: tp.Dict) -> pd.DataFrame:
+    df = df.copy()
+    outliers_columns = params["columns"]
+    df_train_outliers_clean = handle_outliers_iqr(
+        df=df, columns=outliers_columns, k=params["k"], method=params["method"]
+    )
+    return df_train_outliers_clean
--- src/bank_transaction_user_classification/pipelines/data_preproses/pipeline.py
+++ src/bank_transaction_user_classification/pipelines/data_preproses/pipeline.py
@@ -3,8 +3,21 @@ This is a boilerplate pipeline 'data_preproses'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
-
+from kedro.pipeline import Node, Pipeline, node  # noqa
+from .nodes import handling_outliers
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        node(
+            func=handling_outliers,
+            inputs=["train_df", "params:handling_outliers"],
+            outputs="train_df_cleaned",
+            name="train_df_cleaned_node",
+        ),
+        node(
+            func=handling_outliers,
+            inputs=["test_df", "params:handling_outliers"],
+            outputs="test_df_cleaned",
+            name="test_df_cleaned_node",
+        )
+    ])
