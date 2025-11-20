8857dff19001448745da5acd2b6b910133459658
TurmaeUrsorum <projekbrillylutfan@gmail.com>
2025-11-20 19:13:21 +0700

    update untuk kedro runner menggunakan pararel exc

--- src/bank_transaction_user_classification/pipeline_registry.py
+++ src/bank_transaction_user_classification/pipeline_registry.py
@@ -2,6 +2,11 @@
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+from .pipelines.data_cleaning import pipeline as dc
+from .pipelines.data_EDA import pipeline as de
+from .pipelines.data_preproses import pipeline as dp
+from .pipelines.data_modeling import pipeline as dm
+from .pipelines.data_evaluasi import pipeline as de
 
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -10,6 +15,22 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+
+    data_cleaning_pipeline = dc.create_pipeline()
+    data_EDA_pipeline = de.create_pipeline()
+    data_preproses_pipeline = dp.create_pipeline()
+    data_modeling_pipeline = dm.create_pipeline()
+    data_evaluasi_pipeline = de.create_pipeline()
+
+    return {
+        "__default__": data_cleaning_pipeline
+        + data_EDA_pipeline
+        + data_preproses_pipeline
+        + data_modeling_pipeline
+        + data_evaluasi_pipeline,
+        "dc": data_cleaning_pipeline,
+        "de": data_EDA_pipeline,
+        "dp": data_preproses_pipeline,
+        "dm": data_modeling_pipeline,
+        "de": data_evaluasi_pipeline,
+    }
